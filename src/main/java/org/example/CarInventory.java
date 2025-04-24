af13332f6d091a394408877fd24f6ca7e3990e63
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-24 00:07:45 -0400

    Complete Tests

--- src/main/java/org/example/CarInventory.java
+++ src/main/java/org/example/CarInventory.java
@@ -1,38 +1,50 @@
 package org.example; 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CarInventory {
-   
+
+    HashMap<String, Car> carInventory;
 
     public CarInventory() {
-      
+        carInventory = new HashMap<>();
     }
 
     public void addCar(Car car) {
-     
+        if (car == null) throw new IllegalArgumentException("Car parameter can not be null");
+        if (carInventory.containsKey(car.getModel())) throw new IllegalArgumentException("Cars can not be the same model");
+        carInventory.put(car.getModel(), car);
     }
 
     public Car getCar(String model) {
-      
+        return carInventory.get(model);
     }
 
     public Car sellCar(String model) {
-      
+        return carInventory.remove(model);
     }
 
     public void updateCarPrice(String model, double newPrice) {
-      
+        Car carToUpdate = this.getCar(model);
+        if (carToUpdate != null) {
+            carToUpdate.setPrice(newPrice);
+        }
     }
 
     public List<Car> findCarsByMake(String make) {
-    
+        List<Car> carsWithSimilarMake = new ArrayList<>();
+        for (String carModelKey : carInventory.keySet()) {
+            Car currentCar = carInventory.get(carModelKey);
+            String currentCarMake = carInventory.get(carModelKey).getMake();
+            if (Objects.equals(currentCarMake, make)) {
+               carsWithSimilarMake.add(currentCar);
+            }
+        }
+
+        return carsWithSimilarMake;
     }
 
     public int getCarCount() {
-        
+       return carInventory.size();
     }
 
     public static void main( String[] args )
